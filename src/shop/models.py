a2eeab76afb47db158dfdf5eed5113e5b992fbb4
youyouYoung <youyou.young.mine@gmail.com>
2025-01-13 17:27:30 -0600

    temporarily set category field can be null

--- src/shop/models.py
+++ src/shop/models.py
@@ -31,7 +31,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     brand = models.CharField(max_length=100, default='xyz')
     current_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
     discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
