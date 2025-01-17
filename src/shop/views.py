452eb840f4368aaa87f5aa9ce044d1f20d2f058a
youyouYoung <youyou.young.mine@gmail.com>
2025-01-16 22:04:58 -0600

    delete unused import

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from .models import Product, Order, Category, ProductImage
 from .serializers import ProductSerializer, OrderSerializer, CategorySerializer, ProductImageSerializer
 
