4bc41bbe12f29302f3b616acaefebec3de725373
youyouYoung <youyou.young.mine@gmail.com>
2025-02-06 20:18:11 -0600

    add index.html to catch up / request

--- shop_project/shop_project/urls.py
+++ shop_project/shop_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('auth/', include('auth.urls')),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ]
